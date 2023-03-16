d156b39005923cdbf45ecfb89028682bc23917d9
Eli Moshe <moshebogo@gmail.com>
2023-03-16 02:25:33 -0400

    runs sitl by default

---
+++ connection_template.py
@@ -0,0 +1,28 @@
+from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException
+import argparse
+import time
+import socket
+import pyception
+import math
+
+def connectMyCopter():
+
+    parser = argparse.ArgumentParser(description='commands')
+    parser.add_argument('--connect')
+    args = parser.parse_args()
+
+    connection_string = args.connect
+
+    if not connection_string:
+        import dronekit_sitl
+        sitl = dronekit_sitl.start_default()
+        connection_string = sitl.connection_string()
+        
+
+
+    Vehicle = connect(connection_string, wait_ready=True)
+    print(dronekit_sitl)
+
+    return Vehicle
+
+vehicle = connectMyCopter()
