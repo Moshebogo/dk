13ea91372bf0b0b309d2e25493ebeaf0d47e5478
Eli Moshe <moshebogo@gmail.com>
2023-04-13 23:47:38 -0400

    added prints

--- arm.py
+++ arm.py
@@ -17,20 +17,23 @@ print( str(vehicle.system_status.state) )
 #####################   arming the drone   ###########################
 
 
-def arm ():
-    while vehicle.is_armable!=True:
-        print("Waiting for vehicle to become armable.")
-        time.sleep(1)
-    print("Vehicle is now armable.")
-    print("") 
+def arm():
+    print("vehicle mode: ", vehicle.mode)
+    # while vehicle.is_armable!=True:
+    #     print("Waiting for vehicle to become armable.")
+    #     time.sleep(1)
+    # print("Vehicle is now armable.")
+    # print("") 
 
     vehicle.mode = VehicleMode("GUIDED")
+    print("vehicle mode: ", vehicle.mode)
     while vehicle.mode!="GUIDED":
         print("Waiting for drone to enter GUIDED mode.")
         time.sleep(1)
     print("Vehicle is now in GUIDED mode")
 
     vehicle.armed = True
+    print("vehicle armed: ", vehicle.armed)
     while vehicle.armed==False:
         print("Waiting for vehicle to be armed.")
         time.sleep(1)
