d54ec8ad23f9a16cfa7befb261d8223cb363ed23
Eli Moshe <moshebogo@gmail.com>
2023-04-27 23:23:32 -0400

    header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,21 @@
-import './App.css';
+import '/home/eli_moshe/Development/code/dk/client/src/App.css';
 import {useState} from "react"
 import Buttons from './buttons';
 import MavproxyForm from './MavproxyForm';
 import Register from './Register';
+import Header from './Header';
 
 
 export default  function App() {
 
 
   return (
-    <>
+    <div>
+      <Header />
       <Buttons />
       <MavproxyForm />
       <Register />
-    </>
+    </div>
   )
 }
 
