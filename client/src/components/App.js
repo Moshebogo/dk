2af40c991622a7321b415abb3808306e67530273
Eli Moshe <moshebogo@gmail.com>
2023-05-10 22:07:11 -0400

    11

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,7 +12,7 @@ import SavedRoutes from './SavedRoutes';
 export default  function App() {
 
 const [stateUser, setUser] = useState(false)
-const x = 10
+const x = 11
 
   return (
     <BrowserRouter>
