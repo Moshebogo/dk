70331c6bb691ce2187cbcb0b239b424e0ba966cc
Eli Moshe <moshebogo@gmail.com>
2023-05-25 19:30:52 -0400

    stuff

--- server/stuff.py
+++ server/stuff.py
@@ -2,13 +2,4 @@ def set_yaw(heading):
     print("set_yaw function") 
     
 
-
-def stuff(stuff):
-    print(stuff)
-
-
-def stuff2(stuff):
-    print(stuff)
-
-def stuff3(stuff):
-    print(stuff)
+f)
