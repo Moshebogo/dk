29322f99621345919f8bf4e687113ae80f689a0c
Eli Moshe <moshebogo@gmail.com>
2023-07-09 18:51:09 -0700

    started google sign in

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,31 +1,72 @@
-import '/home/eli_moshe/Development/code/dk/client/src/App.css';
-import {useState} from "react"
-import {  BrowserRouter, Routes, Route} from "react-router-dom"
-import TestConnection from './TestConnection';
-import MavproxyForm from './MavproxyForm';
-import Header from './Header';
-import Home from './Home';
-import Map from './Map';
-import SavedRoutes from './Routes';
-
-export default  function App() {
-
-const [stateUser, setUser] = useState(false)
-const [userData, setUserData] = useState("")
-// testing
-const [markers, setMarker] = useState( [] )
 
 
-  return (
-    <BrowserRouter>
-      <Header stateUser={stateUser}/>
-        <Routes>
-            <Route path="/"                element={<Home setMarker={setMarker} />}/>
-            <Route path="/testConnection"  element={<TestConnection />}/> 
-            <Route path="/map"             element={<Map markers={markers} setMarker={setMarker} />}/>
-            <Route path="/savedroutes"     element={<SavedRoutes stateUser={stateUser} setUser={setUser} userData={userData} setUserData={setUserData} />}/>
-            <Route path="/mavproxyform"    element={<MavproxyForm />}/>   
-        </Routes>
-    </BrowserRouter>   
-  )
+
+import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+
+function App() {
+    const responseMessage = (response) => {
+        console.log(response);
+    };
+    const errorMessage = (error) => {
+        console.log(error);
+    };
+    return (
+        <div>
+            <h2>React Google Login</h2>
+            <br />
+            <br />
+            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+        </div>
+    )
 }
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import '/home/eli_moshe/Development/code/dk/client/src/App.css';
+// import {useState} from "react"
+// import {  BrowserRouter, Routes, Route} from "react-router-dom"
+// import TestConnection from './TestConnection';
+// import MavproxyForm from './MavproxyForm';
+// import Header from './Header';
+// import Home from './Home';
+// import Map from './Map';
+// import SavedRoutes from './Routes';
+
+// export default  function App() {
+
+// const [stateUser, setUser] = useState(false)
+// const [userData, setUserData] = useState("")
+// // testing
+// const [markers, setMarker] = useState( [] )
+
+
+//   return (
+//     <BrowserRouter>
+//       <Header stateUser={stateUser}/>
+//         <Routes>
+//             <Route path="/"                element={<Home setMarker={setMarker} />}/>
+//             <Route path="/testConnection"  element={<TestConnection />}/> 
+//             <Route path="/map"             element={<Map markers={markers} setMarker={setMarker} />}/>
+//             <Route path="/savedroutes"     element={<SavedRoutes stateUser={stateUser} setUser={setUser} userData={userData} setUserData={setUserData} />}/>
+//             <Route path="/mavproxyform"    element={<MavproxyForm />}/>   
+//         </Routes>
+//     </BrowserRouter>   
+//   )
+// }
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+// this is for the google sign in
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="1069590200077-0o6auit6vfs7slh081gskaq00fnul4ec.apps.googleusercontent.com">
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </GoogleOAuthProvider>
+   
+    // <React.StrictMode>
+    //         <App />
+    //     </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
